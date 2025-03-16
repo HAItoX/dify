fd31420df00be41b013a03853efa30a6fa4ae6c7
htdong <dht31261@gmail.com>
2025-03-16 21:15:16 +0800

    web add basepath

--- web/next.config.js
+++ web/next.config.js
@@ -14,6 +14,7 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/hitoflow/web',
   webpack: (config, { dev, isServer }) => {
     config.plugins.push(codeInspectorPlugin({ bundler: 'webpack' }))
     return config
